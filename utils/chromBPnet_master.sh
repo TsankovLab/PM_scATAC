f08babcbeb0aa2da1f3bd3a2aa22247a3de01b27
Brawni <brunogiotti@gmail.com>
2024-12-23 10:58:41 -0500

    updates

--- utils/chromBPnet_master.sh
+++ utils/chromBPnet_master.sh
@@ -75,6 +75,7 @@ bsub -J wait_jobs -P acc_Tsankov_Normal_Lung -w "$job_ids"  -o wait.log -e wait.
 ### Combine contribution scores 
 echo "combine contribution scores"
 ml anaconda3/2020.11
+conda env list
 source activate h5py # activate another environment with hdf5plugin installed to read h5 files
 python $repodir/utils/average_CNT_scores.py $chromBPct_dir $celltype
 
