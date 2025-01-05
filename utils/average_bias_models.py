d2540831a5c5c41bdaaf39e085ca4fe358df1dd6
Brawni <brunogiotti@gmail.com>
2025-01-05 18:39:47 -0500

    updates

--- utils/average_bias_models.py
+++ utils/average_bias_models.py
@@ -5,7 +5,7 @@ import os
 import hdf5plugin
 
 # Get variables from sys.argv
-# chromBPct_dir = sys.argv[1]  # First argument
+chromBPct_dir = sys.argv[1]  # First argument
 # celltype = sys.argv[2]
 #var2 = sys.argv[2]  # Second argument
 #var3 = sys.argv[3]  # Third argument
